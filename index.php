f78dfe9253cbc0a35e1f573924f4ea84a5832723
Joseph Kwon <99274059+JosephK2006@users.noreply.github.com>
2022-06-08 02:21:19 +0000

    updated index.html

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="Factorials in JS">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Joseph Kwon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="fav/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fav/favicon-16x16.png">
+    <link rel="manifest" href="fav/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <!-- title -->
+    <title>Factorials</title> 
+  </head>
+  <body>
+    <h1>Factorials using Do While Loops</h1>
+    <?php echo "<p>Enter the required information below: </p>" ?>
+    <!-- Style -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- function -->
+    <form action="./results.php" method="post" target="result">
+      <!-- text-field -->
+      <label for="number">Number:</label>
+      <input type="number" id="number" placeholder="Enter Number" min="0"><br><br>
+      <!-- button -->
+      <input type="submit" value="Calculate">
+    </form> <br>
+    <!-- Create a space where the information will be displayed -->
+    
+		<!-- result frame centered -->
+    <div id="result"></div>
+    <center>
+      <iframe id="result" name="result">
+      </iframe>
+    </center>
+    
+  </body>
+</html>
---
+++ results.php
@@ -0,0 +1,23 @@
+<?php
+
+//variables
+$answer = 1;
+
+// get the user's first integer 
+$userNum = intval($_POST["number"]);
+
+if ($userNum > 0){
+  //do while loop
+do {
+  //build the equation for the factorial
+  $answer = $answer * $userNum;
+  $userNum = $userNum - 1;
+
+} while ($userNum > 0);
+
+
+
+//display the answer
+echo $answer;
+
+?>
